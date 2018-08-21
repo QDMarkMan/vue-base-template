c5e3d249120571e3bcd0903243b654900fc85e77
unknown <5231202571@qq.com>
2018-08-21 14:21:10 +0800

    update webpackV4

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === 'production'){
   // 核心依赖包
   externals['vue'] = 'Vue'
   externals['axios'] = 'Axios'
-  externals['vuex'] = 'vuex'
+  externals['Vuex'] = 'vuex'
   externals['iview'] = 'iview'
   // externals['jquery'] = 'jQuery'
 }
