23b549d1ced4439e51b4a9e9ce1903d9d0db7679
etongfu <531202571@qq.com>
2019-03-20 17:15:53 +0800

    完善

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Author: etongfu
+ * @Email: 13583254085@163.com
+ * @LastEditors: etongfu
+ * @Description: 列表页之间的骨架屏
+ * @youWant: add you want info here
+ * @Date: 2019-03-20 16:49:02
+ * @LastEditTime: 2019-03-20 16:52:12
+ -->
+ <template>
+   <div class="skeleton-component">
+     
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+  name: "skeleton", 
+  data () {
+    open: false
+  },
+  methods: {
+  toggleSkeleton(status) {
+    this.open = status
+  } 
+  },
+}
+</script>
+<style lang="less">
+  
+</style>
+ 
