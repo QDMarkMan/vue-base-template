e0985d986a673a54fb4de2e730998f31c697ceec
etongfu <531202571@qq.com>
2019-05-27 16:05:58 +0800

    预览版本升级

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,8 @@ import commonRouter from './common'
 import toppingRouter from './topping'
 Vue.use(VueRouter)
 export default new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  /* mode: 'history',
+  base: process.env.BASE_URL, */
   scrollBehavior: () => ({ y: 0 }),
   // the real routers
   routes: toppingRouter.concat([...RouterConfig, ...commonRouter])
