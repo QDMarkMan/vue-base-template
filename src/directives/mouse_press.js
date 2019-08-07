65d4ccc9203459980d46bd0ff6052027ac2b8e68
etongfu <tongfu.e@xtalpi.com>
2019-08-07 09:48:11 +0800

    edit: 修改指令注释

--- src/directives/mouse_press.js
+++ src/directives/mouse_press.js
@@ -3,8 +3,8 @@
  * @Email: 13583254085@163.com
  * @Date: 2019-07-31 13:54:22
  * @LastEditors: etongfu
- * @LastEditTime: 2019-07-31 13:55:12
- * @Description: 
+ * @LastEditTime: 2019-08-07 09:42:42
+ * @Description: 鼠标长按指令, 绑定value需要是个function
  * @YouWant: add you want info here
  */
 const listeners = (timer, binding) => {
@@ -13,7 +13,6 @@ const listeners = (timer, binding) => {
     if (e.type === 'click') {
       return
     }
-    console.log('long press start')
     if (timer === null) {
       timer = setTimeout(() => {
         binding.value(true) // 告诉宿主鼠标已经开始执行
