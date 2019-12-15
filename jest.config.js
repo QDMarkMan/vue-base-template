b6f198fa16a29b6bcd375b97653aaffc1fe4e516
etongfu <etongfu@etongfudeMacBook-Pro.local>
2019-12-15 11:49:38 +0800

    update: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@
 const path = require('path');
 
 module.exports = {
-  rootDir: path.resolve(__dirname, '../../'), // 同 webpack.context
+  // rootDir: path.resolve(__dirname, './'), // 同 webpack.context
   moduleFileExtensions: [ // 同 webpack.resolve.extensions
     'js',
     'json',
@@ -16,11 +16,11 @@ module.exports = {
   //   '^@/(.*)$': '<rootDir>/src/$1', // 同 webpack.resolve.alias
   // },
   transform: { // 同 webpack.module.rules
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
   },
-  setupFiles: ['<rootDir>/tests/unit/setup'], // 同 webpack.entry
-  coverageDirectory: '<rootDir>/tests/unit/coverage', // 同 webpack.output
+  // setupFiles: ['<rootDir>/tests/unit/setup'], // 同 webpack.entry
+  // coverageDirectory: '<rootDir>/tests/unit/coverage', // 同 webpack.output
   // collectCoverageFrom: [ // 同 webpack 的 rule.include
   //   'src/**/*.{js,vue}',
   //   '!src/main.js',
